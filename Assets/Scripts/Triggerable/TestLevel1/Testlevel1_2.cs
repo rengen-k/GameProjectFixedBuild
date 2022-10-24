a38635f8c41957082322c57bb98f0ec51a8ee629
rengen-k <harrowby1@hotmail.co.uk>
2022-10-24 20:37:45 +0100

    updated level

---
+++ Assets/Scripts/Triggerable/TestLevel1/Testlevel1_2.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testlevel1_2 : Triggerable
+{
+    private bool isTrigger;
+    private Quaternion nextRotation;
+
+    public float speed = 0.1f; //speed in degrees per second?
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTrigger) {
+            // transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * Quaternion.Euler(0, 90 ,0), 20 * Time.deltaTime);
+            transform.rotation = transform.rotation * Quaternion.Euler(0, 90, 0);
+                isTrigger = false;
+        }
+        
+    }
+
+    public override void triggerAct(){
+        Debug.Log("Puzzle activated");
+        isTrigger = true;
+    }
+}
