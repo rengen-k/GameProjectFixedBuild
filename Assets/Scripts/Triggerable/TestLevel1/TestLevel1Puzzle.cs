83debba03b9fa695d5a46a2e8a964fef0c9f9fc0
rengen-k <harrowby1@hotmail.co.uk>
2022-10-31 14:08:43 +0000

    added slerp on puzzle pieces for level 9

--- Assets/Scripts/Triggerable/TestLevel1/TestLevel1Puzzle.cs
+++ Assets/Scripts/Triggerable/TestLevel1/TestLevel1Puzzle.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 
 public class TestLevel1Puzzle : Triggerable
 {
-    private bool isTrigger;
+    private bool isTrigger = true;
     private Quaternion nextRotation;
-
-    public float speed = 0.1f; //speed in degrees per second?
+    private bool rotate = false;
+    public float journeyTime = 1.0f;
+    private float startTime;
     
     // Start is called before the first frame update
     void Start()
     {
+        nextRotation = transform.rotation * Quaternion.Euler(0, 90, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
- 
-    }
+        if (rotate) {
+            transform.rotation = Quaternion.Slerp(transform.rotation, nextRotation, Time.deltaTime * 8);
+            if (transform.rotation == nextRotation) {
+                rotate = false;
+            }
 
-    public override void triggerAct(){
-        if (isTrigger){
+        }
+    }
+    
+
+    public override void triggerAct()
+    {
+        if (isTrigger) {
+            startTime = Time.time;
             isTrigger = false;
             Debug.Log("rotate");
-            transform.rotation = transform.rotation * Quaternion.Euler(0, 90, 0);
+            if (rotate) {
+                nextRotation = nextRotation * Quaternion.Euler(0, 90, 0);
+            } else {
+                nextRotation = transform.rotation * Quaternion.Euler(0, 90, 0);
+            }
+            rotate = true;
+            Debug.Log(nextRotation);
         }
     }
 
