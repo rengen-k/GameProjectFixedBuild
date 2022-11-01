bd2e5e83156b8dc02d436385e14b8874d03cd494
rengen-k <harrowby1@hotmail.co.uk>
2022-11-01 16:10:18 +0000

    first commit

---
+++ Assets/Scripts/WaypointPath_Linear.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Source: https://www.youtube.com/watch?v=ly9mK0TGJJo
+
+public class WaypointPath_Linear : MonoBehaviour
+{
+    public Transform GetWaypoint(int waypointIndex)
+    {
+        return transform.GetChild(waypointIndex);
+    }
+
+    public int GetNextWaypointIndex(int currentWaypointIndex)
+    {
+        int nextWaypointIndex = currentWaypointIndex + 1;
+
+        if (nextWaypointIndex == transform.childCount)
+        {
+            nextWaypointIndex = 0;
+        }
+
+        return nextWaypointIndex;
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # GameDevGrpProjectAM
 
 Credits
+
 Unity Object Fade by jhuthins
 https://github.com/jhutchines/Unity-Object-Fade
+
 PlayerInputErrorWorkaround
 https://forum.unity.com/threads/type-of-instance-in-array-does-not-match-expected-type.1320564/#post-8396772
+
+Moving Platforms
+https://www.youtube.com/watch?v=ly9mK0TGJJo
