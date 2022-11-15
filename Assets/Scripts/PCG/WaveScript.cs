f55e8e22966123efbd05ebdf217a4dc6946973b9
Candle-Wick <56155870+Candle-Wick@users.noreply.github.com>
2022-11-15 15:45:07 +0000

    Wavey blocks, NOT CREDITED

---
+++ Assets/Scripts/PCG/WaveScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// TODO GET PROPER CREDTIS FOR THIS
+// Credit to Lecturer
+
+public class WaveScript : MonoBehaviour {
+    [Tooltip("Scale of waves, the height change.")]
+    [SerializeField] private float scale = 0.7f;
+    [Tooltip("The speed of change.")]
+    [SerializeField] private float speed = 3.0f;
+    private Vector3[] baseHeight;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Waves();
+		
+	}
+
+    void Waves()
+    {
+        //Get current cube mesh
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        if (baseHeight == null)
+            baseHeight = mesh.vertices;
+
+        //Vertices for cube mesh
+        Vector3[] vertices = new Vector3[baseHeight.Length];
+
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = baseHeight[i];
+
+            //Sinus value for rolling waves
+            float value = Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale;
+
+            //Add sinus and update the vertex
+            vertex.y += value;
+            vertices[i] = vertex;
+
+        }
+
+        //Update cube mesh
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+
+
+    }
+}
