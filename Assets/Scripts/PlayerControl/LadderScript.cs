cb4c858826eec093c336e1491068a72704187a6c
cegroen <ce12groen@gmail.com>
2022-11-16 16:41:34 +0000

    player snaps to ladder

--- Assets/Scripts/PlayerControl/LadderScript.cs
+++ Assets/Scripts/PlayerControl/LadderScript.cs
@@ -23,6 +23,8 @@ public class LadderScript : MonoBehaviour
     private float deceleration = 40;
     RigidbodyConstraints originalConstraints;
     private PlayerController playerController;
+    private Vector3 tempPos;
+    private float offset = 0.6f;
 
     void Start()
     {
@@ -37,7 +39,6 @@ public class LadderScript : MonoBehaviour
     {
         playerActionsScript = new PlayerActionsScript();
         playerActionsScript.Player.Enable();
-        //playerActionsScript.Player.Interact.performed += Interact;
         //camScript = GameObject.Find("StateDrivenCamera").GetComponent<CameraSwitchScript>();
     }
 
@@ -54,34 +55,103 @@ public class LadderScript : MonoBehaviour
         inputVector = playerActionsScript.Player.Move.ReadValue<Vector2>();
 
         /* same movement as in PlayerController but for up and down. I initially tried using rb.movePosition
-        but it ignored collisions so I went back to adding forces. */
+        but it ignored collisions so I went back to adding forces */
         float targetSpeed = inputVector.y * ladderSpeed;
         float speedDif = targetSpeed - rb.velocity.y;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
         float move = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
         ladderMovement.y = move;
 
-        /* allows you to move up and down and also applies friction if you are within distance of a ladder
-        and you have clicked interact. Also constrains rigidbody */
-        if (inLadder)
+        // says you are on the ladder if you are within range and have clicked the up or down input
+        if (inLadder && inputVector.y != 0)
         {
-            //rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-            //rb.AddForce(ladderMovement * Time.fixedDeltaTime);
-            //float amount = Mathf.Min(Mathf.Abs(rb.velocity.y), Mathf.Abs(frictionAmount));
-            //amount *= Mathf.Sign(rb.velocity.y);
-            //rb.AddForce(Vector3.up * -amount, ForceMode.Impulse);
+            onLadder = true;
+            tempPos = transform.position;
+            Collider[] ladders = Physics.OverlapSphere(ladderCheck.position, ladderRadius, (int)whatIsLadder);
+            //if (ladders[0] != null)
+            //{
+            //    if (ladders[0].transform.rotation == Quaternion.Euler(0, 0, 0) && transform.position.x < ladders[0].transform.position.x)
+            //    {
+            //        tempPos.x = ladders[0].transform.position.x - offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, 90, 0) && transform.position.z < ladders[0].transform.position.z)
+            //    {
+            //        tempPos.z = ladders[0].transform.position.z - offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, 180, 0) && transform.position.x > ladders[0].transform.position.x)
+            //    {
+            //        tempPos.x = ladders[0].transform.position.x + offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, -90, 0) && transform.position.z > ladders[0].transform.position.z)
+            //    {
+            //        tempPos.z = ladders[0].transform.position.z + offset;
+            //        transform.position = tempPos;
+            //    }
 
-            if (inputVector.y != 0)
+            //}
+
+            foreach (Collider ladder in ladders)
             {
-                onLadder = true;
+                if (ladder.transform.rotation == Quaternion.Euler(0, 0, 0) && transform.position.x < ladder.transform.position.x)
+                {
+                    tempPos.x = ladder.transform.position.x - offset;
+                    transform.position = tempPos;
+                }
+                else if (ladder.transform.rotation == Quaternion.Euler(0, 90, 0) && transform.position.z < ladder.transform.position.z)
+                {
+                    tempPos.z = ladder.transform.position.z - offset;
+                    transform.position = tempPos;
+                }
+                else if (ladder.transform.rotation == Quaternion.Euler(0, 180, 0) && transform.position.x > ladder.transform.position.x)
+                {
+                    tempPos.x = ladder.transform.position.x + offset;
+                    transform.position = tempPos;
+                }
+                else if (ladder.transform.rotation == Quaternion.Euler(0, -90, 0) && transform.position.z > ladder.transform.position.z)
+                {
+                    tempPos.z = ladder.transform.position.z + offset;
+                    transform.position = tempPos;
+                }
             }
         }
+
+        /* allows you to move up and down and also applies friction if you are within distance of a ladder
+        and you have clicked up or down */
         if (inLadder && onLadder && !playerController.isJumping)
         {
             rb.AddForce(ladderMovement * Time.fixedDeltaTime);
             float amount = Mathf.Min(Mathf.Abs(rb.velocity.y), Mathf.Abs(frictionAmount));
             amount *= Mathf.Sign(rb.velocity.y);
             rb.AddForce(Vector3.up * -amount, ForceMode.Impulse);
+
+            //tempPos = transform.position;
+            //Collider[] ladders = Physics.OverlapSphere(ladderCheck.position, ladderRadius, (int)whatIsLadder);
+            //if (ladders[0] != null)
+            //{
+            //    if (ladders[0].transform.rotation == Quaternion.Euler(0, 0, 0) && transform.position.x < ladders[0].transform.position.x)
+            //    {
+            //        tempPos.x = ladders[0].transform.position.x - offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, 90, 0) && transform.position.z < ladders[0].transform.position.z)
+            //    {
+            //        tempPos.z = ladders[0].transform.position.z - offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, 180, 0) && transform.position.x > ladders[0].transform.position.x)
+            //    {
+            //        tempPos.x = ladders[0].transform.position.x + offset;
+            //        transform.position = tempPos;
+            //    }
+            //    else if (ladders[0].transform.rotation == Quaternion.Euler(0, -90, 0) && transform.position.z > ladders[0].transform.position.z)
+            //    {
+            //        tempPos.z = ladders[0].transform.position.z + offset;
+            //        transform.position = tempPos;
+            //    }
+            //}
         }
     }
 
@@ -94,27 +164,15 @@ public class LadderScript : MonoBehaviour
             rb.constraints = originalConstraints;
             rb.useGravity = true;
         }
+        // turns off gravity while on a ladder so you don't slide down
         if (onLadder)
         {
             rb.useGravity = false;
+            ladderRadius = 0.8f;
         }
-        Debug.Log("onLadder: " + onLadder);
+        else
+        {
+            ladderRadius = 1.25f;
+        }
     }
-
-    //// if you click the interact button and are close enough to the ladder you will be able to climb it
-    //public void Interact(InputAction.CallbackContext context)
-    //{
-    //    if (inLadder && !onLadder)
-    //    {
-    //        onLadder = true;
-    //        rb.constraints = rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-    //        rb.useGravity = false;
-    //    }
-    //    else if (onLadder)
-    //    {
-    //        onLadder = false;
-    //        rb.constraints = originalConstraints;
-    //        rb.useGravity = true;
-    //    }
-    //}
 }
