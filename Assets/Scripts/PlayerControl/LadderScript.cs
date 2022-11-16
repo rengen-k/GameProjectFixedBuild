cfd8c96ed2d4a718302c99e548eb202620869e24
cegroen <ce12groen@gmail.com>
2022-11-16 00:50:49 +0000

    Movement with forces instead
    movePosition disregards collisions so I used forces like our normal movement

--- Assets/Scripts/PlayerControl/LadderScript.cs
+++ Assets/Scripts/PlayerControl/LadderScript.cs
@@ -8,14 +8,19 @@ public class LadderScript : MonoBehaviour
 {
     private bool inLadder;
     private bool onLadder;
-    public float ladderSpeed;
+    [SerializeField] private float frictionAmount = 2f;
+    [SerializeField] private float ladderSpeed = 10000;
     private Vector3 ladderMovement;
     private Vector2 inputVector;
     private Rigidbody rb;
-    private float ladderRadius = 0.9f;
+    private float ladderRadius = 1.5f;
     private PlayerActionsScript playerActionsScript;
+    //public CameraSwitchScript camScript;
     [SerializeField] private Transform ladderCheck;
     [SerializeField] private LayerMask whatIsLadder;
+    private float velPower = 1.5f;
+    private float acceleration = 17;
+    private float deceleration = 25;
 
     void Start()
     {
@@ -29,6 +34,7 @@ public class LadderScript : MonoBehaviour
         playerActionsScript = new PlayerActionsScript();
         playerActionsScript.Player.Enable();
         playerActionsScript.Player.Interact.performed += Interact;
+        //camScript = GameObject.Find("StateDrivenCamera").GetComponent<CameraSwitchScript>();
     }
 
     private void OnDisable()
@@ -40,6 +46,21 @@ public class LadderScript : MonoBehaviour
     {
         inLadder = Physics.CheckSphere(ladderCheck.position,ladderRadius, (int) whatIsLadder);
         inputVector = playerActionsScript.Player.Move.ReadValue<Vector2>();
+
+        float targetSpeed = inputVector.y * ladderSpeed;
+        float speedDif = targetSpeed - rb.velocity.y;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        float move = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
+        ladderMovement.y = move;
+
+        if (inLadder && onLadder)
+        {
+            rb.AddForce(ladderMovement * Time.fixedDeltaTime);
+            float amount = Mathf.Min(Mathf.Abs(rb.velocity.y), Mathf.Abs(frictionAmount));
+            amount *= Mathf.Sign(rb.velocity.y);
+            rb.AddForce(Vector3.up * -amount, ForceMode.Impulse);
+            //rb.MovePosition(transform.position + ladderMovement * ladderSpeed * Time.fixedDeltaTime);
+        }
     }
 
     void Update()
@@ -47,16 +68,20 @@ public class LadderScript : MonoBehaviour
         if (!inLadder)
         {
             onLadder = false;
-            GetComponent<PlayerController>().enabled = true;
+            //GetComponent<PlayerController>().enabled = true;
             rb.useGravity = true;
         }
 
-        if (inLadder && onLadder)
-        {
-            ladderMovement = new Vector3(0f, inputVector.y, 0f);
-            //rb.AddForce(ladderMovement * 1000 * Time.fixedDeltaTime);
-            rb.MovePosition(transform.position + ladderMovement * ladderSpeed * Time.fixedDeltaTime);
-        }
+        //if (inLadder && onLadder)
+        //{
+        //    ladderMovement = new Vector3(0f, inputVector.y, 0f);
+        //    rb.AddForce(ladderMovement * ladderSpeed * Time.deltaTime);
+        //    float amount = Mathf.Min(Mathf.Abs(rb.velocity.y), Mathf.Abs(frictionAmount));
+        //    amount *= Mathf.Sign(rb.velocity.y);
+        //    rb.AddForce(Vector3.up * -amount, ForceMode.Impulse);
+        //    //rb.MovePosition(transform.position + ladderMovement * ladderSpeed * Time.fixedDeltaTime);
+        //}
+
         Debug.Log("inLadder: " + inLadder);
         Debug.Log("onLadder: " + onLadder);
     }
@@ -66,13 +91,13 @@ public class LadderScript : MonoBehaviour
         if (inLadder && !onLadder)
         {
             onLadder = true;
-            GetComponent<PlayerController>().enabled = false;
+            //GetComponent<PlayerController>().enabled = false;
             rb.useGravity = false;
         }
         else if (onLadder)
         {
             onLadder = false;
-            GetComponent<PlayerController>().enabled = true;
+            //GetComponent<PlayerController>().enabled = true;
             rb.useGravity = true;
         }
     }
