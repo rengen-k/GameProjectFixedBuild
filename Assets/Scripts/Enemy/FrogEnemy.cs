6193067ce4f21d624f54723e9a317c45de6ef393
cegroen <ce12groen@gmail.com>
2022-11-21 03:23:00 +0000

    cleaning up and commenting script

--- Assets/Scripts/Enemy/FrogEnemy.cs
+++ Assets/Scripts/Enemy/FrogEnemy.cs
@@ -3,29 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-//-----------------------------------------//
-// EnemyAI
-//-----------------------------------------//
-// Some code from https://www.youtube.com/watch?v=c8Nq19gkNfs
-// Enemy AI, handles enemy movement.
-
 public class FrogEnemy : MonoBehaviour
 {
+    // game objects and components
     private Transform player;
     public Transform respawnPoint;
     private NavMeshAgent agent;
-    //The list of waypoints- gameobjects, it will try to travel to.
+    private Rigidbody rb;
+
+    //The list of waypoints - gameobjects, it will try to travel to.
     public Transform[] waypoints;
+
+    // general enemy variables
     private int waypointIndex;
     private Vector3 target;
     private float originalSpeed;
-    private Rigidbody rb;
+
+    // frog related variables
     private bool grounded = true;
     private bool canJump = true;
     [SerializeField] private float jumpHeight;
     [SerializeField] private float jumpDist;
     [SerializeField] private float jumpTime;
-    private Vector3 currentPos;
 
     [Tooltip("Indicates whether the enemy will follow the player when they are close enough.")]
     [SerializeField] private bool followsPlayer;
@@ -43,6 +42,7 @@ public class FrogEnemy : MonoBehaviour
 
     private void Update()
     {
+        // stops the agent teleporting backwards after jumping
         if (agent.updatePosition == false)
         {
             agent.nextPosition = transform.position;
@@ -51,6 +51,7 @@ public class FrogEnemy : MonoBehaviour
         {
             Jump();
         }
+        // increases speed when following player
         if (followsPlayer && Vector3.Distance(transform.position, player.position) < followPlayerDist)
         {
             agent.SetDestination(player.position);
@@ -70,6 +71,7 @@ public class FrogEnemy : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // increases gravity slightly
         if (rb.useGravity == true)
         {
             rb.AddForce(Physics.gravity * 1.2f, ForceMode.Acceleration);
@@ -82,6 +84,7 @@ public class FrogEnemy : MonoBehaviour
         agent.SetDestination(target);
     }
 
+    // called when enemy approaches a waypoint so it can continue to the next one
     private void IterateWaypointIndex()
     {
         waypointIndex++;
@@ -101,17 +104,21 @@ public class FrogEnemy : MonoBehaviour
         grounded = false;
         if (agent.enabled)
         {
+            // deactivates NavMeshAgent to allow it to jump
             agent.updatePosition = false;
             agent.updateRotation = false;
             agent.isStopped = true;
         }
         rb.useGravity = true;
+
+        // jumps up and in the direction the agent was moving prior to being deactivated
         rb.AddForce((Vector3.up * jumpHeight) + (agent.velocity.normalized * jumpDist), ForceMode.Impulse);
         StartCoroutine(JumpCooldown());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // ground detection
         if (collision.gameObject.layer == LayerMask.NameToLayer("Ground") || collision.gameObject.layer == LayerMask.NameToLayer("StableGround"))
         {
             if (!grounded)
@@ -119,6 +126,7 @@ public class FrogEnemy : MonoBehaviour
                 grounded = true;
                 if (agent.enabled)
                 {
+                    // reactivates NavMeshAgent once on the ground
                     agent.updatePosition = true;
                     agent.updateRotation = true;
                     agent.isStopped = false;
@@ -126,13 +134,17 @@ public class FrogEnemy : MonoBehaviour
                 rb.useGravity = false;
             }
         }
+        else if (collision.gameObject.name == "KillPlane")
+        {
+            Respawn();
+        }
     }
 
+    // controls how often the enemy jumps
     private IEnumerator JumpCooldown()
     {
         canJump = false;
         yield return new WaitForSeconds(jumpTime);
         canJump = true;
-        UpdateDestination();
     }
 }
