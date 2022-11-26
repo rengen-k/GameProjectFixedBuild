4f27e3e2bdd9d65ef7a254422af9d56cd3ba93d5
cegroen <ce12groen@gmail.com>
2022-11-26 23:29:11 +0000

    Trampoline enemy gets squashed when you jump on its head and then it becomes a trampoline

--- Assets/Scripts/Enemy/TrampolineEnemy.cs
+++ Assets/Scripts/Enemy/TrampolineEnemy.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TrampolineEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool squashed = false;
+    private bool squashCooldown = false;
+    private Vector3 squashedScale = new Vector3(1f, 0.5f, 1f);
+    private Vector3 normalScale = new Vector3(1f, 1f, 1f);
+    private NavMeshAgent agent;
+
+    private void Start()
     {
-        
+        agent = GetComponentInParent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (squashed)
+        {
+            agent.updatePosition = false;
+            agent.updateRotation = false;
+            agent.isStopped = true;
+        }
+        else
+        {
+            squashed = false;
+            transform.parent.transform.localScale = normalScale;
+            gameObject.tag = "Ground";
+            agent.updatePosition = true;
+            agent.updateRotation = true;
+            agent.isStopped = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (!squashed)
+            {
+                squashed = true;
+                transform.parent.transform.localScale = squashedScale;
+                gameObject.tag = "JumpTag";
+                StartCoroutine(SquashCooldown());
+            }
+            //else if (squashed && !squashCooldown)
+            //{
+            //    squashed = false;
+            //    transform.parent.transform.localScale = normalScale;
+            //    gameObject.tag = "Ground";
+            //}
+        }
+    }
+
+    private IEnumerator SquashCooldown()
+    {
+        squashed = true;
+        yield return new WaitForSeconds(4);
+        squashed = false;
     }
 }
