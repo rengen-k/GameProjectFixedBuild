931d8340e58c58655295d06d4c28ee6c9c599838
cegroen <ce12groen@gmail.com>
2022-12-15 20:48:04 +0000

    Separated perspective platform script into 2 scripts
    Also got the player to move correctly relative to the platoforms

--- Assets/Scripts/Level Specific Scripts/PerspectivePlatforms.cs
+++ Assets/Scripts/Level Specific Scripts/PerspectivePlatforms.cs
@@ -11,12 +11,14 @@ public class PerspectivePlatforms : MonoBehaviour
     private float smoothTime = 0.2f;
     private Vector3 velocity = Vector3.zero;
     private Vector3 originalPos;
-    [SerializeField] private bool tower;
     [SerializeField] private float tower0;
     [SerializeField] private float tower1;
     [SerializeField] private float tower2;
     [SerializeField] private float tower3;
-    private bool onBlock;
+    private bool camChange;
+    private float lastCamPos = 0;
+    private Vector3 playerLocalPos;
+    private static bool onBlock;
 
     private void Start()
     {
@@ -25,44 +27,20 @@ public class PerspectivePlatforms : MonoBehaviour
         originalPos = transform.position;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        if (!tower && !onBlock)
+        if (lastCamPos != camScript.cameraPos)
         {
-            if (camScript.cameraPos == 0 || camScript.cameraPos == 2)
-            {
-                target = new Vector3(originalPos.x, originalPos.y, player.position.z);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
-            else if (camScript.cameraPos == 1 || camScript.cameraPos == 3)
-            {
-                target = new Vector3(player.position.x, originalPos.y, originalPos.z);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
+            camChange = true;
+            lastCamPos = camScript.cameraPos;
         }
-
-        if (tower)
+        else
         {
-            if (camScript.cameraPos == 0)
-            {
-                target = new Vector3(originalPos.x, originalPos.y, tower0);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
-            else if (camScript.cameraPos == 1)
-            {
-                target = new Vector3(tower1, originalPos.y, originalPos.z);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
-            else if (camScript.cameraPos == 2)
-            {
-                target = new Vector3(originalPos.x, originalPos.y, tower2);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
-            else if (camScript.cameraPos == 3)
-            {
-                target = new Vector3(tower3, originalPos.y, originalPos.z);
-                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
-            }
+            camChange = false;
+        }
+        if (camChange)
+        {
+            StartCoroutine(moveBlocks());
         }
     }
 
@@ -80,6 +58,54 @@ public class PerspectivePlatforms : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             onBlock = false;
+        }
+    }
+
+    private IEnumerator moveBlocks()
+    {
+        if (camScript.cameraPos == 0 || camScript.cameraPos == 2)
+        {
+            if (camScript.cameraPos == 0 && onBlock)
+            {
+                target = new Vector3(originalPos.x, originalPos.y, tower0);
+            }
+            else if (camScript.cameraPos == 2 && onBlock)
+            {
+                target = new Vector3(originalPos.x, originalPos.y, tower2);
+            }
+            else
+            {
+                target = new Vector3(originalPos.x, originalPos.y, player.position.z);
+            }
+            playerLocalPos = player.localPosition;
+            while (Vector3.Distance(transform.position, target) > 0.1)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+                player.localPosition = playerLocalPos;
+                yield return null;
+            }
+        }
+        else if (camScript.cameraPos == 1 || camScript.cameraPos == 3)
+        {
+            if (camScript.cameraPos == 1 && onBlock)
+            {
+                target = new Vector3(tower1, originalPos.y, originalPos.z);
+            }
+            else if (camScript.cameraPos == 3 && onBlock)
+            {
+                target = new Vector3(tower3, originalPos.y, originalPos.z);
+            }
+            else
+            {
+                target = new Vector3(player.position.x, originalPos.y, originalPos.z);
+            }
+            playerLocalPos = player.localPosition;
+            while (Vector3.Distance(transform.position, target) > 0.1)
+            {
+                transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+                player.localPosition = playerLocalPos;
+                yield return null;
+            }
         }
     }
 }
