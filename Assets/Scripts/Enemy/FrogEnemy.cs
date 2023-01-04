ea107b57273fd2b33f7b77961458204e1723ab65
cegroen <ce12groen@gmail.com>
2023-01-03 22:21:51 -0700

    Moved block and fixed spawn position in level 11

--- Assets/Scripts/Enemy/FrogEnemy.cs
+++ Assets/Scripts/Enemy/FrogEnemy.cs
@@ -244,7 +244,7 @@ public class FrogEnemy : MonoBehaviour
     private IEnumerator AttackCooldown()
     {
         attackCooldown = true;
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(3);
         attackCooldown = false;
     }
 
