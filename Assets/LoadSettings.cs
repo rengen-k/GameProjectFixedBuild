d2b30c84d3346bc2f37cf8213416705582afdfaa
omk88 <Oldmanjenkins56@gmail.com>
2023-01-07 23:05:13 +0000

    Persistent Post Processing Effects

--- Assets/LoadSettings.cs
+++ Assets/LoadSettings.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering.PostProcessing;
 
 public class LoadSettings : MonoBehaviour
 {
@@ -10,10 +11,74 @@ public class LoadSettings : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     private InputActionRebindingExtensions.RebindingOperation rebindingOperation;
 
+    public PostProcessVolume Volume;
+    private Bloom bloomEffect;
+    private AmbientOcclusion ambientOcclusionEffect;
+    private AutoExposure autoExposureEffect;
+    private ChromaticAberration chromaticAberrationEffect;
+    private Grain grainEffect;
+    private Vignette vignetteEffect;
+    private MotionBlur motionBlurEffect;
+    private LensDistortion lensDistortionEffect;
+    private DepthOfField depthOfFieldEffect;
+
     void Start()
     {
         playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
         currentSettings = GameObject.FindGameObjectWithTag("CurrentSettings").GetComponent<CurrentSettings>();
+        Volume = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PostProcessVolume>();
+        Volume.profile.TryGetSettings(out bloomEffect);
+        Volume.profile.TryGetSettings(out ambientOcclusionEffect);
+        Volume.profile.TryGetSettings(out autoExposureEffect);
+        Volume.profile.TryGetSettings(out chromaticAberrationEffect);
+        Volume.profile.TryGetSettings(out grainEffect);
+        Volume.profile.TryGetSettings(out vignetteEffect);
+        Volume.profile.TryGetSettings(out motionBlurEffect);
+        Volume.profile.TryGetSettings(out lensDistortionEffect);
+        Volume.profile.TryGetSettings(out depthOfFieldEffect);
+
+        bloomEffect.active = currentSettings.BloomOn;
+        bloomEffect.intensity.value = currentSettings.BloomIntensity;
+        bloomEffect.threshold.value = currentSettings.BloomThreshold;
+        bloomEffect.softKnee.value = currentSettings.BloomSoftKnee;
+        bloomEffect.clamp.value = currentSettings.BloomClamp;
+        bloomEffect.diffusion.value = currentSettings.BloomDiffusion;
+        bloomEffect.anamorphicRatio.value = currentSettings.BloomAnamorphicRatio;
+
+        ambientOcclusionEffect.active = currentSettings.AmbientOcclusionOn;
+        ambientOcclusionEffect.intensity.value = currentSettings.AmbientOcclusionIntensity;
+        ambientOcclusionEffect.thicknessModifier.value = currentSettings.AmbientOcclusionThickness;
+
+        chromaticAberrationEffect.active = currentSettings.ChromaticAberrationOn;
+        chromaticAberrationEffect.intensity.value = currentSettings.ChromaticAberrationIntensity;
+
+        grainEffect.active = currentSettings.GrainOn;
+        grainEffect.intensity.value = currentSettings.GrainIntensity;
+        grainEffect.size.value = currentSettings.GrainSize;
+        grainEffect.lumContrib.value = currentSettings.GrainLuminanceContribution;
+
+        vignetteEffect.active = currentSettings.VignetteOn;
+        vignetteEffect.intensity.value = currentSettings.VignetteIntensity;
+        vignetteEffect.smoothness.value = currentSettings.VignetteSmoothness;
+        vignetteEffect.roundness.value = currentSettings.VignetteRoundness;
+
+        motionBlurEffect.active = currentSettings.MotionBlurOn;
+        motionBlurEffect.shutterAngle.value = currentSettings.MotionBlurShutterAngle;
+        motionBlurEffect.sampleCount.value = (int) currentSettings.MotionBlurSampleCount;
+
+        lensDistortionEffect.active = currentSettings.LensDistortionOn;
+        lensDistortionEffect.intensity.value = currentSettings.LensDistortionIntensity;
+        lensDistortionEffect.intensityX.value = currentSettings.LensDistortionXMultiplier;
+        lensDistortionEffect.intensityY.value = currentSettings.LensDistortionYMultiplier;
+        lensDistortionEffect.centerX.value = currentSettings.LensDistortionCenterX;
+        lensDistortionEffect.centerY.value = currentSettings.LensDistortionCenterY;
+        lensDistortionEffect.scale.value = currentSettings.LensDistortionScale;
+
+        depthOfFieldEffect.active = currentSettings.DepthOfFieldOn;
+        depthOfFieldEffect.focusDistance.value = currentSettings.DepthOfFieldFocusDistance;
+        depthOfFieldEffect.aperture.value = currentSettings.DepthOfFieldAperture;
+        depthOfFieldEffect.focalLength.value = currentSettings.DepthOfFieldFocalLength;
+        
         Action = playerController.playerActionsScript.Player.Jump;
         Action.Disable();
         Action.ApplyBindingOverride(currentSettings.Jump);
