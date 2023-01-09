eea3fc9afc45b362bfc421bc22a290bbcbf49fe8
omk88 <Oldmanjenkins56@gmail.com>
2023-01-09 03:45:33 +0000

    Level Changes

--- Assets/Scripts/PuzzleBlock7.cs
+++ Assets/Scripts/PuzzleBlock7.cs
@@ -19,7 +19,7 @@ public class PuzzleBlock7 : Triggerable
     private int turns = 0;
     private int turns2 = 250;
     public GameObject triggerCube;
-    float magnitude = 7f;
+    float magnitude = 10f;
 
 
     void Start()
